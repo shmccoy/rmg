de5df3c8029e1653415548947e8cc25560fb0b79
Sean M <53903852+shmccoy@users.noreply.github.com>
2020-07-30 00:29:47 -0400

    Create MemeGenerator and Edit App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MemeGenerator from "./MemeGenerator";
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return <MemeGenerator />;
+  }
+}
+
+export default App;
